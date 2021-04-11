90d4b6612cf5dc26a1fa43603f6e29cc1d69dfad
MehediHasan25 <ishanmehadi@gmail.com>
2021-04-11 16:58:33 +0600

    bank asia uat deployment version 2.2.0

--- src/component/E-KYC/Simplified/Liveness/Camera.js
+++ src/component/E-KYC/Simplified/Liveness/Camera.js
@@ -34,8 +34,8 @@ export class Camera extends Component {
     libImage.capture(
       "videoCap",
       "imageCap",
-      // "http://103.17.69.111/models/"
-      "https://ovichowdhury.github.io/face-api-models/"
+      "http://103.17.69.111/models/"
+      // "https://ovichowdhury.github.io/face-api-models/"
       // "https://agent1.bankasia-bd.com:3000/models/"
     );
     this.libImage = libImage;
